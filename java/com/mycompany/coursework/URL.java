14e924fdc3a7bfd10c0b4c5cc7aa7ee4695616e7
CheemsEnjoyer <95558369+CheemsEnjoyer@users.noreply.github.com>
2024-06-01 21:27:59 +0800

    Delete java directory

--- java/com/mycompany/coursework/URL.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.coursework;
-
-
-public class URL {
-    public static final String Url = "jdbc:mysql://localhost:3306/practiceemu";
-}
--- java/domain/port/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package domain.port;
-import java.util.List;
-import domain.entity.Company;
-public interface CompanyRepository {
-    List<Company> loadData(String inputData);
-    Company fetchCompanyByName(String companyName);
-}
