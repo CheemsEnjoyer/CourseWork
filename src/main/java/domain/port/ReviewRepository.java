aff95a72d029c75ade5c13fddd1bdd52bee61826
CheemsEnjoyer <95558369+CheemsEnjoyer@users.noreply.github.com>
2024-06-01 21:27:52 +0800

    Delete src directory

--- src/main/java/domain/port/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package domain.port;
-import domain.entity.Review;
-import java.util.List;
-public interface ReviewRepository {
-    List<Review> fetchReviewsByCompanyName(String inputData);
-    boolean insertReviewToDatabase(String reviewText, int rating, int userId, int companyId);
-}
